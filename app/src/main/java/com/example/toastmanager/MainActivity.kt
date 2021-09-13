a99649567e5819cc195c49eb76c487635141c4d0
fuxinbo <fuxinbo@xstread.com>
2021-09-13 14:21:06 +0800

    [需求/BUG/重构/其他]: 需求 [描述]：删除无用依赖 [原因]： [解决方案]： [备注]

--- app/src/main/java/com/example/toastmanager/MainActivity.kt
+++ app/src/main/java/com/example/toastmanager/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.toastmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.toast.ToastManager
+import com.example.toast.toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastManager.instance.init()
+        findViewById<TextView>(R.id.text).setOnClickListener {
+            toast("测试啊")
+        }
     }
 }
